1890a0b9ce9d650a604fe89d44a0027de0b192c9
Felipe Ocampo <felipe.aocampo@gmail.com>
2023-10-07 00:05:30 -0700

    start about section

--- foxy-web-design-and-development/src/components/hero/hero-section.tsx
+++ foxy-web-design-and-development/src/components/hero/hero-section.tsx
@@ -1,10 +1,12 @@
-import Fox from "./Fox";
+import Fox from "./fox";
 
 export default function HeroSection() {
   return (
-    <section className="bg-red-500 text-center mt-2">
+    <section className="bg-red-500 text-center">
       <Fox />
-      <h1></h1>
+      <h1 className="text-4xl max-w-2xl m-auto -mt-4">
+        Exceptional Web Design and Development for Exceptional Clients
+      </h1>
     </section>
   );
 }
--- foxy-web-design-and-development/src/pages/index.tsx
+++ foxy-web-design-and-development/src/pages/index.tsx
@@ -1,7 +1,14 @@
+import AboutSection from "@/components/about/about-section";
+import HeroSection from "@/components/hero/hero-section";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <h1>placeholder</h1>;
+  return (
+    <>
+      <HeroSection />
+      <AboutSection />
+    </>
+  );
 }
