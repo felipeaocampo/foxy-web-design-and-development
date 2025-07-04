edd77938c192504e7f84667b6117da38a3927825
Felipe Ocampo <felipe.ocampo@chainguard.dev>
2025-07-04 17:54:45 -0400

    update repo

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,26 +1,62 @@
-import GoogleAnalytics from "@/components/google-analytics/google-analytics";
+/*
+Copyright 2025 Chainguard, Inc.
+SPDX-License-Identifier: Apache-2.0
+*/
+
+/* eslint-disable */
+
 import { Html, Head, Main, NextScript } from "next/document";
 import Script from "next/script";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" className="scroll-smooth">
       <Head>
-        {/* <GoogleAnalytics GA_MEASUREMENT_ID={"G-C68S2TB5DF"} /> */}
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','${"G-C68S2TB5DF"}');
-        `}
-        </Script>
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+						new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+						j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+						'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+						})(window,document,'script','dataLayer','GTM-58WV5RN')`,
+          }}
+        ></Script>
       </Head>
       <body>
         <Main />
+        <div id="outer-modal"></div>
         <NextScript />
       </body>
     </Html>
   );
 }
+
+// BEFORE STARTING AT CHAINGUARD
+// import GoogleAnalytics from "@/components/google-analytics/google-analytics";
+// import { Html, Head, Main, NextScript } from "next/document";
+// import Script from "next/script";
+
+// export default function Document() {
+//   return (
+//     <Html lang="en">
+//       <Head>
+//         {/* <GoogleAnalytics GA_MEASUREMENT_ID={"G-C68S2TB5DF"} /> */}
+//         <Script id="google-tag-manager" strategy="afterInteractive">
+//           {`
+//         (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+//         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+//         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+//         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+//         })(window,document,'script','dataLayer','${"G-C68S2TB5DF"}');
+//         `}
+//         </Script>
+//       </Head>
+//       <body>
+//         <Main />
+//         <NextScript />
+//       </body>
+//     </Html>
+//   );
+// }
