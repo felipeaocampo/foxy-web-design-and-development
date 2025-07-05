aad2a6d417e5815997b8ef1c5e68f189c2285ef7
Felipe Ocampo <felipe.ocampo@chainguard.dev>
2025-07-05 13:15:21 -0400

    add stape gtm custom loader script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,7 +12,8 @@ export default function Document() {
   return (
     <Html lang="en" className="scroll-smooth">
       <Head>
-        <Script
+        {/* GOOGLE CLOUD RUN (GCP) SGTM CONNECTION SCRIPT */}
+        {/* <Script
           id="google-tag-manager"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
@@ -21,6 +22,14 @@ export default function Document() {
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','GTM-5X25ZN8P');`,
+          }}
+        ></Script> */}
+        {/* STAPE SGTM CONNECTION SCRIPT */}
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s);j.async=true;j.src="https://stape.grimmy.shop/ayrqiphndm.js?"+i;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','eodqo7qa=aWQ9R1RNLTVYMjVaTjhQ&apiKey=34775901');`,
           }}
         ></Script>
       </Head>
